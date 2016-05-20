248db8d7e6fae52a0bfacf2cdd3bc22e7f3ecb9c
Adnan Begovic <adnan@cyngn.com>
2016-05-20 12:57:43 -0700

    fw: Repurpose PREVENT_POWER_KEY for preventing multiple system keys.
      Allows applications like camera, and various testing applications,
      to intercept certain system keys to verify behavior or leverage it
      in creative ways.
    
      Limited to signature/privileged.
    
    Change-Id: Iacd8735af2aabafbf1fc062d8c565b7102dc6865
    TICKET: FEIJ-522
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3548,6 +3548,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 return 0;
             }
 
+            if (mTopFullscreenOpaqueWindowState != null &&
+                    (mTopFullscreenOpaqueWindowState.getAttrs().privateFlags
+                            & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS) != 0
+                    && mScreenOnFully) {
+                return 0;
+            }
+
             if (down) {
                 if (mPressOnMenuBehavior == KEY_ACTION_APP_SWITCH
                         || mLongPressOnMenuBehavior == KEY_ACTION_APP_SWITCH) {
@@ -3620,7 +3627,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             return 0;
         } else if (keyCode == KeyEvent.KEYCODE_APP_SWITCH) {
-
             // Disable app switch key if hw keys is set to off
             if (scanCode != 0 && !hasHwKeysEnabled()) {
                 Log.i(TAG, "Ignoring App Switch Key: we have hw keys disabled");
@@ -6114,6 +6120,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_HOME:
+                if (mTopFullscreenOpaqueWindowState != null &&
+                        (mTopFullscreenOpaqueWindowState.getAttrs().privateFlags
+                                & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS) != 0
+                        && mScreenOnFully) {
+                    return result;
+                }
+
                 if (down && !interactive && mHomeWakeScreen) {
                     isWakeKey = true;
                 }
@@ -6195,7 +6208,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_POWER: {
                 if (mTopFullscreenOpaqueWindowState != null &&
                         (mTopFullscreenOpaqueWindowState.getAttrs().privateFlags
-                        & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY) != 0
+                        & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS) != 0
                         && mScreenOnFully) {
                     return result;
                 }
