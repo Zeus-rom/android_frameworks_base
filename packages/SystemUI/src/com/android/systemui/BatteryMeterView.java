fa9d495ee936cfc47f7d58aa0e97ee50cb2c4b28
Willi Ye <williye97@gmail.com>
2016-06-10 14:41:59 +0200

    BatteryMeterView: Bring back circle animation
    Change-Id: I351c48508bb160464d03ce2aab5dbeb1fd7e3f78

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -18,7 +18,10 @@
 
 package com.android.systemui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -299,6 +302,9 @@ public class BatteryMeterView extends View implements DemoMode,
     public void onBatteryLevelChanged(boolean present, int level, boolean pluggedIn,
             boolean charging) {
         // TODO: Use this callback instead of own broadcast receiver.
+        if (mBatteryMeterDrawable != null) {
+            mBatteryMeterDrawable.onBatteryLevelChanged(level, pluggedIn, charging);
+        }
     }
 
     @Override
@@ -463,6 +469,7 @@ public class BatteryMeterView extends View implements DemoMode,
 
     protected interface BatteryMeterDrawable {
         void onDraw(Canvas c, BatteryTracker tracker);
+        void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging);
         void onSizeChanged(int w, int h, int oldw, int oldh);
         void onDispose();
         void setDarkIntensity(int backgroundColor, int fillColor);
@@ -491,6 +498,8 @@ public class BatteryMeterView extends View implements DemoMode,
 
         private BatteryMeterMode mMode;
         private int mTextGravity;
+
+        private ValueAnimator mAnimator;
 
         public AllInOneBatteryMeterDrawable(Resources res, BatteryMeterMode mode) {
             super();
@@ -550,6 +559,38 @@ public class BatteryMeterView extends View implements DemoMode,
             drawBattery(c, tracker);
             if (mAnimationsEnabled) {
                 // TODO: Allow custom animations to be used
+            }
+        }
+
+        @Override
+        public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
+            if (charging && !isThemeApplied()) {
+                if (mAnimator != null) mAnimator.cancel();
+
+                final int defaultAlpha = mLevelDrawable.getAlpha();
+                mAnimator = ValueAnimator.ofInt(defaultAlpha, 0, defaultAlpha);
+                mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        mLevelDrawable.setAlpha((int) animation.getAnimatedValue());
+                        invalidate();
+                    }
+                });
+                mAnimator.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        mLevelDrawable.setAlpha(defaultAlpha);
+                        mAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mLevelDrawable.setAlpha(defaultAlpha);
+                        mAnimator = null;
+                    }
+                });
+                mAnimator.setDuration(2000);
+                mAnimator.start();
             }
         }
 
