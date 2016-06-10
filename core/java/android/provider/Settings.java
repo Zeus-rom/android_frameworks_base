e550b732d2ab52a2007e510dbf88e692eec1a031
Swapnil Solanki <swapnil133609@gmail.com>
2016-06-10 15:40:24 +0530

    Remove Duplicate field

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5217,12 +5217,6 @@ public final class Settings {
         public static final String BATTERY_FULLY_CHARGED_NOTIFICATION = "battery_fully_charged_notification";
 
         /**
-         * Whether to show the battery fully charged notification
-         * @hide
-         */
-        public static final String BATTERY_FULLY_CHARGED_NOTIFICATION = "battery_fully_charged_notification";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
